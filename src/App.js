4555a95926427a761453abd2accd90354032f872
Anme <belov-toni@list.ru>
2023-07-07 23:23:09 +0300

    without JSX

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 function App() {
-	// императивный стиль, функция и в ней расписываем логику
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="Year">{new Date().getFullYear()}</div>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement(
+				'div',
+				{
+					className: 'Year',
+				},
+				new Date().getFullYear(),
+			),
+		),
 	);
 }
-// текущий год - декларатив, хотим результат - получаем
 export default App;
